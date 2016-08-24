9eb8740d106f314134230686c534feddd36e4d37
ftiar <ftiar@beapi.fr>
2016-08-24 15:17:47 +0200

    Use native wp func word_count
    remove number of chars selected

--- content-character-count.php
+++ content-character-count.php
@@ -1,63 +1,64 @@
 <?php
 /*
-Plugin Name: Count Post Characters (Fork)
-Plugin URI: http://beapi.fr
+Plugin Name: BEA Count Post Characters (Fork)
 Description: Counts characters in real time while you are writing your content. Works for any kind of "post type" out of the box.
 Author: o----o, Florian TIAR
-Version: 1.0
-Author URI: http://tiar-florian.fr
+Version: 1.1
+Author URI: http://beapi.fr
 */
 
+add_action('plugins_loaded', 'CHAR_COUNT_INIT');
 function CHAR_COUNT_INIT() {
 	load_plugin_textdomain( 'CHAR_COUNT', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 }
-add_action('plugins_loaded', 'CHAR_COUNT_INIT');
 
+// Update the char count : Use word count function of WP Core
 add_action( 'admin_print_footer_scripts', 'check_textarea_length' );
-
 function check_textarea_length() {
 	?>
 	<script type="text/javascript">
-		// inject the html
-		jQuery("#wp-word-count").after("<td><?php _e ('Sum of characters:', 'CHAR_COUNT'); ?> <span id=\"ilc_excerpt_counter\"></span>, <?php _e ('Number of selected characters', 'CHAR_COUNT'); ?>: <span id=\"ilc_live_counter\">()</span></td>");
-		// count on load
-		window.onload = function () {
-			var count = 0;
-			var countbis = 1;
-			setInterval(function() {
-				count = tinymce.get('content').getContent().replace(/(\s+)/ig,' ');
-				if ( count != countbis && count != 0 ) {
-					countbis = count;
-					jQuery("#ilc_excerpt_counter").text(count.length);
+		( function( $, counter ) {
+			$("#wp-word-count").after("<td id=\"wp-char-count\"><?php _e ('Sum of characters:', 'CHAR_COUNT'); ?> <span id=\"char-count\">0</span></td>");
+			$( function() {
+				var $content = $( '#content' ),
+					$count = $('#wp-char-count').find( '#char-count' ),
+					prevCount = 0,
+					contentEditor;
+
+				function update() {
+					var text, count;
+
+					if ( ! contentEditor || contentEditor.isHidden() ) {
+						text = $content.val();
+					} else {
+						text = contentEditor.getContent( { format: 'raw' } );
+					}
+
+					count = counter.count( text, 'characters_including_spaces' );
+
+					if ( count !== prevCount ) {
+						$count.text( count );
+					}
+
+					prevCount = count;
 				}
-			}, 500);
-		}
+
+				$( document ).on( 'tinymce-editor-init', function( event, editor ) {
+					if ( editor.id !== 'content' ) {
+						return;
+					}
+
+					contentEditor = editor;
+
+					editor.on( 'nodechange keyup', _.debounce( update, 1000 ) );
+				} );
+
+				$content.on( 'input keyup', _.debounce( update, 1000 ) );
+
+				update();
+			} );
+		} )( jQuery, new wp.utils.WordCounter() );
+
 	</script>
 	<?php
-}
-
-// add function to the tinymce on init
-add_filter( 'tiny_mce_before_init', 'my_tinymce_setup_function' );
-function my_tinymce_setup_function( $initArray ) {
-	$initArray['setup'] = 'function(ed) {
-	    ed.on("keyup", function(e) {
-	        editor_content = ed.getContent().replace(/(\s+)/ig, " ");
-	        jQuery("#ilc_excerpt_counter").text(editor_content.length);
-	    });
-	}';
-	return $initArray;
-}
-
-// add function to the tinymce on init
-add_filter( 'tiny_mce_before_init', 'live_selection_char_count' );
-function live_selection_char_count( $initArray ) {
-	$initArray['setup'] = 'function(ed, index) {
-	    ed.on("click", function(e) {
-	        // get the selection, and strip html
-			 var selection = ed.selection.getContent().replace(/(\s+)/ig, " ");
-			// count the characters and write them in input field
-	       jQuery("#ilc_live_counter").text( "("+selection.length+")");
-	    });
-	}';
-	return $initArray;
 }
---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== Content character count live ===
+Plugin Name: BEA Count Post Characters (Fork)
+Description: Counts characters in real time while you are writing your content. Works for any kind of "post type" out of the box.
+Contributors: o----o, Florian TIAR
+Tags: content, character, admin, live
+Requires at least: 3.4.1
+Tested up to: 4.3.5
+Stable tag: 1.1
+
+Counts charaters live while you write your content. Works for any kind of "post type" out of the box.
+
+== Description ==
+
+Did you ever wanted to know how many characters you type in the content area?
+Now you know.
+
+Credits go to: <a href="http://konstruktors.com/blog/wordpress/3685-limit-number-words-characters-in-wordpress-editor/">Konstructor</a> and <a href="http://www.ilovecolors.com.ar/character-counter-excerpt-wordpress/">ilovecolors</a>.
+
+== Screenshots ==
+ 
+1. Screenshot
+
+== Change log ==
+
+ = 1.1
+ * Edit : Use native function of WordPress (word count) to count chars with spaces
+ * Edit : Remove number of selected characters (useless)
+ * Tweak : Better performances
+ * Tweak : Remove useless code
+
+ = 1.0
+ * New : Add French translation
+ * Tweak : The count of signs is now in real time (every 500ms) when you're editing you're content (only update the count if the content lenght has changed)
+ * Tweak : Also count the white spaces (signs = characters + white spaces)
+ * Tweak : Move markup location to avoid fixed layout (you had to scroll to the bottom of the post, it was annoying for long posts)
+
+ = 0.1.3 =
+ * New : show number of selected characters
+
+ = 0.1.2 =
+ * New : Add Serb/Croatian translation
+
+ = 0.1.1 =
+ * New : Add Spanish translation
+
+ = 0.1 =
+ * initial version
+
+ 
+
